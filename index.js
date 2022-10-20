4a4a1273b6a269eef30c484199bcbdf9385c331d
Austin James <austinjames@Austins-MacBook-Air.local>
2022-10-20 19:17:08 -0400

    finish questions

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ for(let i = 0; i < weeklyExpenseQuestions.length; i++){
     let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
     weeklyExpenses = weeklyExpenses + answer
 }
+for(let i = 0; i < monthlyExpenseQuestions.length; i++){
+    let answer2 = parseFloat(window.prompt(monthlyExpenseQuestions[i]))
+    monthlyExpenses = monthlyExpenses + answer2
+
+}
+
+for(let i = 0; i < annualExpenseQuestions.length; i++){
+    let answer3 = parseFloat(window.prompt(annualExpenseQuestions[i]))
+    annualExpenses = annualExpenses + answer3
+}
